c7b369749b5a78854997c9fa3639221ed08839c9
oakular <callum@oakular.com>
2016-11-17 21:13:16 +0000

    added ClientMain.java

---
+++ src/ClientMain.java
@@ -0,0 +1,9 @@
+public class ClientMain {
+
+    public static void main(String[] args){
+
+        ClientInstance client = new ClientInstance();
+        client.start();
+
+    } // end of main() method
+} // end of ClientMain Class
