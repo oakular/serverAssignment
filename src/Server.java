0d722c04fec2edec1d13013ab40afee2b5c08866
Oakular <cal.warrilow@gmail.com>
2016-11-17 13:10:54 +0000

    begun implementation of socket creation via threads

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,47 @@
 package assignm2;
+import java.net.*;
+import java.net.ServerSocket;
+import java.io.*;
 
 public class Server {
 
-	public Server() {
-	}
+    public static void main(String[] args) throws IOException {
 
+        // if statement to check that port number is valid
+        if (args.length != 1) {
+            System.err.println("Usage: java Server <port number>");
+            System.exit(1);
+        } // end of if statement
+
+        int portNum = Integer.parseInt(args[0]);
+
+        try(ServerSocket serverSocket = new ServerSocket(portNum)){
+            // --- while loop to listen to connection requests and
+            // start them as new threads
+            while(true){
+                Socket clientSocket = serverSocket.accept();
+                MultipleServer multiSever = new MultipleServer(clientSocket);
+                multiSever.start();
+            } // end of while loop
+        } catch (IOException e) {
+            System.err.println("Unable to reach port " + portNum);
+            System.exit(1);
+        } // end of IOException catch
+	} // end of main() method
+
+}
+
+class MultipleServer extends Thread {
+    private Socket multiSocket;
+
+    public MultipleServer(Socket multiSocket){
+        this.multiSocket = multiSocket;
+    } // end of CONSTRUCTOR
+
+    public void run(){
+        PrintWriter pWriter = new PrintWriter(multiSocket.getOutputStream(), true);
+        InputStreamReader streamReader = new InputStreamReader(multiSocket.getInputStream());
+        BufferedReader bReader = new BufferedReader(streamReader);
+
+    }
 }
